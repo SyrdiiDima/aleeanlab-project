87ad53977d9d7e31d60f242a65d580b629f7052a
SyrdiiDima <dimasyrdij@icloud.com>
2022-11-22 17:48:46 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,16 @@
-// import { fetchJobList } from './api/api';
-
-import { Routes, Route } from 'react-router-dom';
-import { JobList } from './pages/JobList';
-import { JobInfo } from './pages/JobInfo';
 export const App = () => {
-  // fetchJobList();
-
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<JobList />}></Route>
-        <Route path="/:jobId" element={<JobInfo />}></Route>
-      </Routes>
-    </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
